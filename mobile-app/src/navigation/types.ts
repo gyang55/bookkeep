3ca23f5c892fdec8679733dbf87e495427e807ef
Guang Yang <guangy@emonster.ca>
2025-04-28 22:56:55 -0700

    clear all docs

--- mobile-app/src/navigation/types.ts
+++
@@ -1,9 +0,0 @@
-import { Expense } from '../types';
-
-export type RootStackParamList = {
-  Login: undefined;
-  Home: undefined;
-  AddExpense: undefined;
-  ExpenseDetail: { expense: Expense };
-  Reports: undefined;
-};
